e2109ca0820c03f252f0cf05cbf5b5202786c060
Eric Kariuki <eric.kariuki@dlight.com>
2024-09-14 17:10:32 +0300

    205

---
+++ src/strings/LeetCode205.java
@@ -0,0 +1,37 @@
+package strings;
+
+import java.util.HashMap;
+
+public class LeetCode205 {
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("edgg","addd"));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()){
+            return false;
+        }
+
+        HashMap<Character, Character> sToTMap = new HashMap<>();
+        HashMap<Character, Character> tToSMap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char sChar = s.charAt(i);
+            char tChar = t.charAt(i);
+
+            // Check forward mapping
+            if (sToTMap.getOrDefault(sChar, tChar) != tChar) {
+                return false;
+            }
+            sToTMap.putIfAbsent(sChar, tChar);
+
+            // Check reverse mapping
+            if (tToSMap.getOrDefault(tChar, sChar) != sChar) {
+                return false;
+            }
+            tToSMap.putIfAbsent(tChar, sChar);
+        }
+
+        return true;
+    }
+}
